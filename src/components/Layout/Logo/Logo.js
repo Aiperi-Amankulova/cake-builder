b5019a309b72a3fb7690af64f1bbcd43dd68273d
Aiperi <aip.amankulova@gmail.com>
2020-03-28 22:20:30 +0600

    Add basic styling

---
+++ src/components/Layout/Logo/Logo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import logoCss from './Logo.module.css';
+
+export default () => {
+    return (
+      <div className= { logoCss.Logo}> 
+      
+      </div>
+    );
+  }
+
