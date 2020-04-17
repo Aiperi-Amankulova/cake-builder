de96d6e081c83258779651f23e84a47cc3b8d950
Aiperi <aip.amankulova@gmail.com>
2020-04-17 22:55:08 +0600

    Add callbacks to modal conponent

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import classes from './Modal.module.css';
 
-export default ({ children }) => {
+export default ({ children, show}) => {
+  const  modalClasses = [classes.Modal];
+  if (show){
+    modalClasses.push(classes.show)
+  }
+   
     return (
-      <div className={classes.Modal}>
-      {children}
-      Hello
+      <div className={modalClasses.join(' ')} >
+      {children} 
       </div>
     );
   }
