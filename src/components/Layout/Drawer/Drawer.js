da5c8a6488d84138f2c052caf44c5f8daf689171
Aiperi <aip.amankulova@gmail.com>
2020-04-18 23:52:46 +0600

    Add Logo and Navigation to Drawer component

--- src/components/Layout/Drawer/Drawer.js
+++ src/components/Layout/Drawer/Drawer.js
@@ -1,11 +1,13 @@
 import React from 'react'; 
 import classes from './Drawer.module.css';
+import Logo from '../../UI/Logo/Logo'; 
+import Navigation from '../../Toolbar/Navigation/Navigation';
 
 export default () => {
     return (
       <div className={classes.Drawer}> 
-      
-      Sidebar
+        <Logo/> 
+        <Navigation/>
       </div>
     );
   }
