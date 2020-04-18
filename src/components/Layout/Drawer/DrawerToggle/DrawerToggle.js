f672bd48d5634a2ab90b62a3bfcc9f89e02a1c2f
Aiperi <aip.amankulova@gmail.com>
2020-04-19 01:13:42 +0600

    Add new component DrawerToggle

---
+++ src/components/Layout/Drawer/DrawerToggle/DrawerToggle.js
@@ -0,0 +1,10 @@
+import React from 'react'; 
+import classes from './DrawerToggle.module.css';
+
+export default () => { 
+    return (
+      <div className={classes.DrawerToggle}>
+        menu 
+      </div>
+    );
+  }
