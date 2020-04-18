6cc93bcba8c77b9a6e07ffb8f0ca15c5aa160921
Aiperi <aip.amankulova@gmail.com>
2020-04-19 00:44:52 +0600

    Add Drawer State

--- src/components/Layout/Drawer/Drawer.js
+++ src/components/Layout/Drawer/Drawer.js
@@ -4,11 +4,13 @@ import Logo from '../../UI/Logo/Logo';
 import Navigation from '../../Toolbar/Navigation/Navigation'; 
 import Backdrop from '../../UI/Backdrop/Backdrop';
 
-export default () => {
+export default ({ toggleDrawer, open}) => {
+  const contentClasses = [classes.content];
+  contentClasses.push(open ? classes.Open : classes.Closed);
     return (
       <div className={classes.Drawer}>
-        <Backdrop show/>
-        <div className={classes.content}> 
+        <Backdrop hideCallback={toggleDrawer} show={open}/>
+        <div className={contentClasses.join(' ')}> 
            <Logo/> 
            <Navigation/>
         </div>  
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Toolbar from '../Toolbar/Toolbar'; 
 import classes from './Layout.module.css'; 
 import Drawer from '../Layout/Drawer/Drawer';
 
 export default ({children}) => {
+  const [drawerOpen, setDrawerOpen]=useState(false);
+
     return (
       <div className= {classes.Layout}> 
        <Toolbar/>  
-       <Drawer/>
+       <Drawer open={drawerOpen} toggleDrawer={()=>{}}/>
       <main>{children}</main> 
       </div>
 
