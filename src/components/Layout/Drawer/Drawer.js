42d938055f6a4369c746229cbf6175d196c9285d
Aiperi <aip.amankulova@gmail.com>
2020-04-19 00:24:04 +0600

    Add Backdrop to Drawer component

--- src/components/Layout/Drawer/Drawer.js
+++ src/components/Layout/Drawer/Drawer.js
@@ -1,13 +1,17 @@
 import React from 'react'; 
 import classes from './Drawer.module.css';
 import Logo from '../../UI/Logo/Logo'; 
-import Navigation from '../../Toolbar/Navigation/Navigation';
+import Navigation from '../../Toolbar/Navigation/Navigation'; 
+import Backdrop from '../../UI/Backdrop/Backdrop';
 
 export default () => {
     return (
-      <div className={classes.Drawer}> 
-        <Logo/> 
-        <Navigation/>
+      <div className={classes.Drawer}>
+        <Backdrop show/>
+        <div className={classes.content}> 
+           <Logo/> 
+           <Navigation/>
+        </div>  
       </div>
     );
   }
