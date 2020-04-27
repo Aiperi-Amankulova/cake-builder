ccd5f5bf7bb663c97740404463dae2f498f2ab12
Aiperi <aip.amankulova@gmail.com>
2020-04-27 11:38:34 +0600

    Changing colors and sizes

--- src/components/CakeBuilder/CakeControls/CakeControl/CakeControl.js
+++ src/components/CakeBuilder/CakeControls/CakeControl/CakeControl.js
@@ -1,23 +1,23 @@
-import React  from 'react'; 
-import classes from './CakeControl.module.css';
+import React from "react";
+import classes from "./CakeControl.module.css";
 
-export default ({ control, addIngredient, removeIngredient, disabled}) => {
- 
-    return (
-      <div className= { classes.CakeControl }> 
-       <button 
-       className={classes.less} 
-       onClick={()=> removeIngredient(control.type)}
-       disabled={disabled}>
-          -
-       </button>
-       <span className={classes.label}>{control.label}</span>
-       <button 
-       className={classes.more} 
-       onClick={()=> addIngredient(control.type)}>
-          +
+export default ({ control, addIngredient, removeIngredient, disabled }) => {
+  return (
+    <div className={classes.CakeControl}>
+      <button
+        className={classes.less}
+        onClick={() => removeIngredient(control.type)}
+        disabled={disabled}
+      >
+        -
       </button>
-      </div>
-    );
-  }
- 
+      <span className={classes.label}>{control.label}</span>
+      <button
+        className={classes.more}
+        onClick={() => addIngredient(control.type)}
+      >
+        +
+      </button>
+    </div>
+  );
+};
