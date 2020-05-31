dbd05a66c86e76f1ffeb9903f9adddad502a0354
Aiperi <aip.amankulova@gmail.com>
2020-05-31 23:22:27 +0600

    Add reducer and actions for orders

---
+++ src/store/reducers/orders.js
@@ -0,0 +1,17 @@
+import * as types from "../actions/types";
+
+const initialState = {
+  orders: false,
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case types.SET_ORDERS:
+      return {
+        ...state,
+        orders: action.orders,
+      };
+    default:
+      return state;
+  }
+};
