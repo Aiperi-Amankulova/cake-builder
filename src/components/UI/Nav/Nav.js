dce348a5f30bf25a37c79e862f1f2791389f7f29
Aiperi <aip.amankulova@gmail.com>
2020-06-06 21:12:04 +0600

    Add auth logic to Nav

--- src/components/UI/Nav/Nav.js
+++ src/components/UI/Nav/Nav.js
@@ -1,14 +1,17 @@
 import React from "react";
 import NavItem from "./NavItem/NavItem";
 import classes from "./Nav.module.css";
+import { useSelector } from "react-redux";
 
 export default () => {
+  const isAuthenticated = useSelector((state) => state.auth.token !== null);
   return (
     <div className={classes.Nav}>
       <ul>
-        <NavItem url="/builder">Cake Builder</NavItem>
-        <NavItem url="/orders">Orders </NavItem>
-        <NavItem url="/auth">Authenticate</NavItem>
+        <NavItem url="/builder"> Cake Builder</NavItem>
+        {isAuthenticated ? <NavItem url="/orders">Orders</NavItem> : null}
+        {!isAuthenticated ? <NavItem url="/auth">Authenticate</NavItem> : null}
+        {isAuthenticated ? <NavItem url="/logout">Logout</NavItem> : null}
       </ul>
     </div>
   );
--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,4 +1,4 @@
-import { AUTH_SUCCESS, AUTH_FAIL, AUTH_LOGOUT } from "./types";
+import { AUTH_SUCCESS, AUTH_FAIL, AUTH_START, AUTH_LOGOUT } from "./types";
 import axios from "axios";
 
 export const start = (dispatch) =>
@@ -18,10 +18,36 @@ export const fail = (dispatch, error) =>
     type: AUTH_FAIL,
     error,
   });
-export const logout = (dispatch) =>
+
+export const logout = (dispatch) => {
+  localStorage.removeItem("token");
+  localStorage.removeItem("id");
+  localStorage.removeItem("expirationDate");
+
   dispatch({
     type: AUTH_LOGOUT,
   });
+};
+
+export const restore = (dispatch) => {
+  const idToken = localStorage.getItem("idToken");
+  const localId = localStorage.getItem("localId");
+
+  if (idToken && localId) {
+    const expirationDate = new Date(localStorage.getItem("expirationDate"));
+    if (expirationDate > new Date()) {
+      success(dispatch, { idToken, localId });
+      timeout(
+        dispatch,
+        (expirationDate.getTime() - new Date().getTime()) / 1000
+      );
+    } else {
+      logout(dispatch);
+    }
+  } else {
+    logout(dispatch);
+  }
+};
 
 export const timeout = (dispatch, seconds) =>
   setTimeout(() => logout(dispatch), seconds * 1000);
@@ -40,6 +66,13 @@ export const auth = (dispatch, method, email, password) =>
       returnSecureToken: true,
     })
     .then(({ data }) => {
+      const expirationDate = new Date(
+        new Date().getTime() + data.expiresIn * 1000
+      );
+      localStorage.setItem("expirationDate", expirationDate);
+      localStorage.setItem("idToken", data.idToken);
+      localStorage.setItem("localId", data.localId);
+
       success(dispatch, data);
       timeout(dispatch, +data.expiresIn);
     })
