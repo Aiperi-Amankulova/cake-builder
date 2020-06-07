8f357985200225d6323ef485cc16050d4d76d960
Aiperi <aip.amankulova@gmail.com>
2020-06-07 18:58:58 +0600

    Restrict access to routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { useEffect } from "react";
 import Layout from "./containers/Layout/Layout";
+import { useDispatch, useSelector } from "react-redux";
+import { restore } from "./store/actions/auth";
 import CakeBuilder from "./containers/CakeBuilder/CakeBuilder";
 import Checkout from "./components/Checkout/Checkout";
 import "./App.css";
@@ -11,34 +13,53 @@ import { useDispatch } from "react-redux";
 
 export default () => {
   const dispatch = useDispatch();
+  const isAuthenticated = useSelector((state) => state.auth.token !== null);
   useEffect(() => {
     restore(dispatch);
   }, [dispatch]);
 
+  let routesOutput = (
+    <Switch>
+      <Route path="/" exact>
+        <Redirect to="/builder" />
+      </Route>
+      <Route path="/builder">
+        <CakeBuilder />
+      </Route>
+      <Route path="/auth">
+        <Auth />
+      </Route>
+      <Redirect to="/" />
+    </Switch>
+  );
+  if (isAuthenticated) {
+    routesOutput = (
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to="/builder" />
+        </Route>
+        <Route path="/builder">
+          <CakeBuilder />
+        </Route>
+        <Route path="/checkout">
+          <Checkout />
+        </Route>
+        <Route path="/orders">
+          <Orders />
+        </Route>
+        <Route path="/auth">
+          <Auth />
+        </Route>
+        <Route path="/logout">
+          <Logout />
+        </Route>
+      </Switch>
+    );
+  }
+
   return (
     <div className="App">
-      <Layout>
-        <Switch>
-          <Route path="/" exact>
-            <Redirect to="/builder" />
-          </Route>
-          <Route path="/builder">
-            <CakeBuilder />
-          </Route>
-          <Route path="/checkout">
-            <Checkout />
-          </Route>
-          <Route path="/orders">
-            <Orders />
-          </Route>
-          <Route path="/auth">
-            <Auth />
-          </Route>
-          <Route path="/logout">
-            <Layout />
-          </Route>
-        </Switch>
-      </Layout>
+      <Layout>{routesOutput}</Layout>
     </div>
   );
 };
